3ab104104e276de909b93bb5faeba7213c3c8e68
Lê Thành Phú <lephu240905@gmail.com>
2025-11-11 21:37:00 +0700

    update

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -14,32 +14,26 @@ export const protectedRoute = (req, res, next) => {
     }
 
     // xác nhận token hợp lệ
-    jwt.verify(
-      token,
-      process.env.ACCESS_TOKEN_SECRET,
-      async (err, decodedUser) => {
-        if (err) {
-          console.error(err);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decodedUser) => {
+      if (err) {
+        console.error(err);
 
-          return res
-            .status(403)
-            .json({ message: "Access token hết hạn hoặc không đúng" });
-        }
-
-        // tìm user
-        const user = await User.findById(decodedUser.userId).select(
-          "-hashedPassword"
-        );
-
-        if (!user) {
-          return res.status(404).json({ message: "người dùng không tồn tại." });
-        }
-
-        // trả user về trong req
-        req.user = user;
-        next();
+        return res
+          .status(403)
+          .json({ message: "Access token hết hạn hoặc không đúng" });
       }
-    );
+
+      // tìm user
+      const user = await User.findById(decodedUser.userId).select("-hashedPassword");
+
+      if (!user) {
+        return res.status(404).json({ message: "người dùng không tồn tại." });
+      }
+
+      // trả user về trong req
+      req.user = user;
+      next();
+    });
   } catch (error) {
     console.error("Lỗi khi xác minh JWT trong authMiddleware", error);
     return res.status(500).json({ message: "Lỗi hệ thống" });
